c458413a22827d093f8fa0c7cdf860fde9e1ad44
vikrant10 <vikrantkumarsingh333@gmail.com>
2022-11-02 09:29:44 +0530

    String

---
+++ Iw1.js
@@ -0,0 +1,25 @@
+//  // Given a string count the number of words in that string
+// let anime = ["virkram is supehero"];
+// let count = 0;
+// for (let i = 0; i <= anime.length - 1; i++) {
+//   if (anime[i] == " ") {
+//     count++;
+//   }
+// }
+
+// if (anime.length != 0) {
+//   console.log("Numbers of words are:", count + 1);
+// } else {
+//   console.log("Numbers of words are", 0);
+// }
+var str = "virkram", "is", "supehero";
+
+var count = 0;
+
+for (var i = 0; i < str.length; i++) {
+
+    count++;
+
+}
+
+console.log(count);
---
+++ Iw2.js
@@ -0,0 +1,29 @@
+// Given an array of string count the overall total number of characters
+
+var str = "virkram is supehero";
+
+var count = 0;
+
+for (var i = 0; i < str.length; i++) {
+
+    count++;
+
+}
+
+console.log(count);racecar
+{
+    r:2,
+    a:2,
+    c:2,
+    e:1
+}
+let ispalindrom = true
+
+let n = str.length
+for(key in Obj){
+  
+  if(Obj[key]%2!=0){
+    ispalindrom = false
+  }
+  
+}
